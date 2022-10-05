b15950e66630b6be122e1c66981ad1db6f4ecd25
André Cedik <andre.cedik@kuehne-nagel.com>
2022-10-05 22:43:30 +0200

    make it possible to use bootstrap tooltips via https://github.com/h-enk/doks/discussions/281#discussioncomment-719197

--- assets/js/index.js
+++ assets/js/index.js
@@ -177,3 +177,8 @@ Source:
     }
   }
 }());
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
